c8cc41433b6abf1df652d14c11ffeb7df649fd60
CocoaZX <zx349872481@gmail.com>
2018-05-20 19:14:58 +0800

    建立模板

---
+++ falsk.py
@@ -0,0 +1,20 @@
+
+# -*-coding:utf-8-*-
+
+from flask import Flask
+
+apps = Flask(__name__)
+
+@apps.route('/')
+# 捕捉url。此处为全url
+def hello_world():
+    return '<h1> Love U Ma!</h1>'
+    # 返回header，只能有一个header
+
+
+if __name__ == '__main__':
+    apps.debug = True;
+    # 调试模式，开发使用
+
+    # app.run()
+    apps.run(host='0.0.0.0', port=1314);
