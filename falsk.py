8dd25679e98534e6e5e964a337aa2542b755fa88
CocoaZX <zx349872481@gmail.com>
2018-06-07 23:27:42 +0800

    注释

--- falsk.py
+++ falsk.py
@@ -11,10 +11,10 @@ def hello_world():
     return '<h1> Love U Ma!</h1>'
     # 返回header，只能有一个header
 
-
+#确认是运行__main__函数才run，而不是导入的时候
 if __name__ == '__main__':
     apps.debug = True;
     # 调试模式，开发使用
     # app.run()
     apps.run(host='0.0.0.0', port=1314);
-    #此处为全IP，端口为1314,®
+    #此处为全IP，端口为1314,®外网可以访问到
