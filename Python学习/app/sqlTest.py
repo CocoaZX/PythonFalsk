ccefd38d386e5a31f326b6fd25d51e70113ad0fb
CocoaZX <zx349872481@gmail.com>
2018-09-14 09:02:16 +0800

    配置服务器

--- Python学习/app/sqlTest.py
+++ Python学习/app/sqlTest.py
@@ -9,7 +9,7 @@ def index():
     return 'index'
 
 #打开数据库
-db = pymysql.connect(host="127.0.0.1",user="root",password="12345678",db="mysql",port=3306)
+db = pymysql.connect(host="47.106.99.78",user="root",password="20Miqianlan",db="mysql",port=3306)
 
 #获取操作游标
 cur = db.cursor()
