c9ee30390f7e4a96d222e90703e8184d6e9b9cfd
CocoaZX <zx349872481@gmail.com>
2018-10-09 18:03:54 +0800

    new

--- README.md
+++
@@ -1,7 +0,0 @@
-# PythonFalsk
-PythonWeb Flask框架实践
-
-本文件为新手练习flask代码。
-
-#注意事项：
-注意文件起名不可以为flask.py。会和Flask本身冲突。
--- flasky/config.py
+++
@@ -1,9 +0,0 @@
-# -- coding: utf-8 --
-# 配置文件
-
-import os
-
-basedir = os.path.abspath(os.path.dirname(__file__))
-
-
-
